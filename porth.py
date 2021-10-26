4461632731744b3f4c86138418f5c51fb23b283f
rexim <reximkut@gmail.com>
2021-10-27 04:54:17 +0700

    Type Checking Call Limit
    Right now our type checking process hangs on recursive functions. It
    is useful to have recursion in a programming language, but it's not
    critical for making Porth self-hosted as of right now. So for now if
    the type checking process makes too many function calls we just fail
    the compilation process suggesting a workaround (disabling the type
    checking by providing the `-unsafe` flag).
    
    We gonna implement proper type checking of the recursive function
    calls a bit later.

--- porth.py
+++ porth.py
@@ -16,6 +16,7 @@ PORTH_EXT = '.porth'
 X86_64_RET_STACK_CAP=8192
 # TODO: INCLUDE_LIMIT should be probably customizable
 INCLUDE_LIMIT=100
+TYPE_CHECK_CALL_LIMIT=500
 SIM_NULL_POINTER_PADDING = 1 # just a little bit of a padding at the beginning of the memory to make 0 an invalid address
 SIM_STR_CAPACITY  = 640_000
 SIM_ARGV_CAPACITY = 640_000
@@ -628,6 +629,9 @@ def type_check_program(program: Program):
         elif op.typ == OpType.PREP_PROC:
             ctx.ip += 1
         elif op.typ == OpType.CALL:
+            if len(ctx.ret_stack) > TYPE_CHECK_CALL_LIMIT:
+                compiler_error_with_expansion_stack(op.token, "Type checking call limit exceeded. Are you trying to do recursion? We don't support type checking recursion properly yet. Consider using `-unsafe` flag for now.")
+                exit(1)
             ctx.ret_stack.append(ctx.ip + 1)
             assert isinstance(op.operand, OpAddr)
             ctx.ip = op.operand
